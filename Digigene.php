0fc445aa820522bb2d111a2f21751917db01b243
Zachd101 <zachary.dawood@ucc.on.ca>
2017-04-27 12:20:02 -0400

    Added Focus for Company

--- Digigene.php
+++ Digigene.php
@@ -17,49 +17,35 @@
 		$pass = "root";
 		$db = "Digigene";
 
+
+		$mysqli = new mysqli("localhost", "root", "root", "Digigene");
+
+		/* check connection */
+		if (mysqli_connect_errno()) {
+    		printf("Connect failed: %s\n", mysqli_connect_error());
+    	exit();
+		}
+
 		// open connection
 		$connection = mysqli_connect($host, $user, $pass) or die ("Unable to connect!");
 
 		// select database
 		mysqli_select_db($connection,$db) or die ("Unable to select database!");
 
-		// create query
-		$query = "SELECT * FROM symbols";
-
-		// execute query
-		$result = mysqli_query($connection, $query) or die ("Error in query: $query. " . mysqli_error());
-
-		// see if any rows were returned
-		if (mysqli_num_rows($result) > 0) {
-
-    		// print them one after another
-    		echo "<table cellpadding=10 border=1>";
-    		while($row = mysqli_fetch_row($result)) {
-        		echo "<tr>";
-				echo "<td>".$row[0]."</td>";
-        		echo "<td>" . $row[1]."</td>";
-        		echo "<td>".$row[2]."</td>";
-				echo "<td><a href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
-        		echo "</tr>";
-    		}
-		    echo "</table>";
-
-		} else {
-			
-    		// print status message
-    		echo "No rows found!";
-		}
-
-		// free result set memory
-		mysqli_free_result($result);
-
 		$AccName = $_POST["AccName"]; 
 		$Password = $_POST["Password"];
 
-
-		echo $AccName;
+		echo $AccName, $Password;
 		
 
+		$sql = 'INSERT INTO Accounts (AccName, Password) 
+		VALUES (AccName, Password)' ;
+
+		if ($conn->query($sql) === TRUE) {
+  		  echo "New record created successfully";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
 
 
 		// close connection
