d6340bbf7adc95acb905af39f5e3bacdf7e98506
Maxn101 <max.noteboom@10539.local>
2017-05-02 13:54:37 -0400

    Navbar and css

---
+++ Navbar.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+	
+<link rel="stylesheet" type="text/css" href="Digigene.css">
+
+	
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a href="#about">About</a></li>
+</ul>
+
+
+</body>
+</html> 
