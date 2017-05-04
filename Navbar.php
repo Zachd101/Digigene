ae2291c0839c9d9ca2e576a6c34a250d23faa984
Maxn101 <max.noteboom@10539.local>
2017-05-04 10:46:02 -0400

    "

--- Navbar.php
+++ Navbar.php
@@ -10,18 +10,20 @@ session_start();
 
 	
 <link rel="stylesheet" type="text/css" href="Digigene.css">
-
 	
 </head>
 <body>
 
 <ul>
+  <img class="logo" style="float:left"; src="Dnaheart.png">
   <li><a href="http://localhost:8888/Digigene/Home.php">Home</a></li>
   <li><a href="http://localhost:8888/Digigene/Help.php">Help</a></li>
   <li><a href="http://localhost:8888/Digigene/About.php">About</a></li>
   <li style="float:right"><a href=""><?php echo $_SESSION['username'];?> </a></li>
   <li style="float:right"><a href="http://localhost:8888/Digigene/Login.php">Log Out</a></li>
+
 </ul>
+
 
 
 </body>
