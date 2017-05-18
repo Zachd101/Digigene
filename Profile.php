4474fd5f8360238e90402e386bd0cc8aff51eeef
Maxn101 <max.noteboom@10539.local>
2017-05-17 20:52:33 -0400

    start

--- Profile.php
+++ Profile.php
@@ -24,6 +24,24 @@ include 'Navbar.php';
 <div class="bubble">
 <h1> <?php echo $username; ?></h1>
 </div>
+<?php 
+	// open connection
+    $connection = mysqli_connect($host, $user, $pass) or die ("Unable to connect!");
 
+    // select database
+    mysqli_select_db($connection,$db) or die ("Unable to select database!");
+	
+	$query = "INSERT INTO accounts(bio, age, disease, gender) VALUES ('$bio', '$age','$disease','$gender')";
+
+    // execute query
+    $result = mysqli_query($connection, $query) or die ("Error in query: $query. ".mysqli_error());
+ 
+     // close connection
+    mysqli_close($connection);
+
+ 
+
+
+?>
 </body>
 </html>
